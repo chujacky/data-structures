2cd3ce1c14e7147093040e9c91268e95647972ab
chujacky <jacky9407@gmail.com>
2018-11-08 11:12:51 -0800

    finished tree codes

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,13 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  
+  // add the addChild and contain methods to tree
+  //using function shared concept
+  extend(newTree, treeMethods);
+  
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,12 +15,46 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  
+  // create a new node 'tree object'
+  // assign the value for the new node
+  // push the new node in the children array
+  
+  var newNode = Tree(value);
+  this.children.push(newNode);
+  
+  
 };
 
 treeMethods.contains = function(target) {
+  //Using BFS technique to visit every node in the tree
+  //using recursion
+  console.log(this.value);
+  if (this.value === target) {
+    return true;
+    
+  } else if (this.children.length === 0) {
+    // node is a leaf
+    return false;
+    
+  } else {
+    //iterate through children
+    for (var i = 0; i < this.children.length; i++) {
+      var exist =  this.children[i].contains(target);
+      if(exist){
+        return true;
+      }
+    }
+    return false;
+  }
+  
 };
 
-
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
