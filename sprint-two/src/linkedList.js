e8cc7d0f8a09f4c434c59b1207085e6bd02773fd
chujacky <jacky9407@gmail.com>
2018-11-07 21:18:12 -0800

    finish linkedlist code

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,46 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    var newNode = Node(value);
+    // make the last node point to the new one if the list is not empty
+    if (list.tail !== null) {
+      var lastNode = this.tail;
+      lastNode.next = newNode;
+    }
+    
+    if ( this.head === null ) {
+      this.head = newNode;
+    }
+    
+    this.tail = newNode;
+
   };
 
   list.removeHead = function() {
+    
+    var firstNode = this.head;
+    this.head = this.head.next;
+    return firstNode.value;
+    
   };
 
   list.contains = function(target) {
+    
+    // visit each node and compair the value to target
+    var pointer = this.head;
+    while ( pointer !== null ) {
+      if ( pointer.value === target ) {
+        return true;
+      }
+      pointer = pointer.next;
+    }
+    return false;
   };
 
   return list;
+  
+  
 };
 
 var Node = function(value) {
