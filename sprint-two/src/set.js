7370f83856c87ed4aa5ac4e10c196cc3884ccf2e
chujacky <jacky9407@gmail.com>
2018-11-09 10:33:05 -0800

    "finished through set"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //complexity time is O(1)
+  if ( !this.contains(item)) {
+    this._storage.push(item);
+  }
+  
+  
 };
 
 setPrototype.contains = function(item) {
+  //complexity time is O(n)
+  var idx = this._storage.indexOf(item);
+  if( idx === -1) {
+    return false;
+  }
+  return true;
 };
 
 setPrototype.remove = function(item) {
+  //complexity time is O(n)
+  var idx = this._storage.indexOf(item);
+  if( idx !== -1) {
+    this._storage.splice(idx,1);
+  }
 };
 
 /*
