a7f034ab61fb62a401b6603e21bfa4411409b608
chujacky <jacky9407@gmail.com>
2018-11-09 20:01:15 -0800

    added test to linkedList spec last node point to null

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -50,6 +50,14 @@ describe('linkedList', function() {
     linkedList.removeHead();
     expect(linkedList.contains(4)).to.equal(false);
   });
+  
+  it('should have last node point to null by -- Jacky & Ammar --', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToTail(6);
+    linkedList.addToTail(7);
+    expect(linkedList.tail.next).to.equal(null);
+  });
 
   // add more tests here to test the functionality of linkedList
 });
